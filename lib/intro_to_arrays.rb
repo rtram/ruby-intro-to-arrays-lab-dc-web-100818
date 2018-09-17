0ae309a59816f9cdc27ae7a4a128c817c07ec675
Robin Tram <robin.tram@gmail.com>
2018-09-17 21:55:28 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,19 @@ end
 def array_with_two_elements
   [1, 2]
 end 
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.shift
+end
